e46438acb5867fe3e11ae91b535662210dc90ea6
Bobby Frazette <Bobby Frazette>
2017-03-06 18:42:23 -0500

    html and css almost done

---
+++ app.js
@@ -0,0 +1,14 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAEkK3tm-ahLHhdMSiXRPy3aRR1V0p7qwE",
+    authDomain: "train-scheduler-2fa26.firebaseapp.com",
+    databaseURL: "https://train-scheduler-2fa26.firebaseio.com",
+    storageBucket: "train-scheduler-2fa26.appspot.com",
+    messagingSenderId: "1092761737972"
+  };
+  firebase.initializeApp(config);
+
+//list current time
+//update times of pre-listed trains
+//create functionality for 
+//check new york times file and time sheet logic file as a reference 
